b422eeb73bbe6449b22c3d058e40bc5e54a7834d
Ahmed Kamel <ahmedk@mailk97.com>
2025-09-20 19:56:30 +0300

    Add user task routes with authentication

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -1,7 +1,20 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserTaskController;
+use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [AuthController::class, 'login']);
 
+Route::middleware(['auth:sanctum', 'role:user'])->group(function () {
+    Route::prefix('tasks')->group(function () {
+        // Users can only retrieve tasks assigned to them
+        Route::get('/', [UserTaskController::class, 'index']);
+        Route::get('/{task}', [UserTaskController::class, 'show']);
+
+        // Users can only update status of tasks assigned to them
+        Route::put('/{task}/status', [UserTaskController::class, 'updateTaskStatus']);
+    });
+
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
