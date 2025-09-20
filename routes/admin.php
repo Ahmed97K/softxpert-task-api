6447dd10a9c78a46f76cf0c14c3e3890c1436f87
Ahmed Kamel <ahmedk@mailk97.com>
2025-09-20 19:02:57 +0300

    Update admin routes to require admin role

--- routes/admin.php
+++ routes/admin.php
@@ -6,14 +6,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [AuthController::class, 'login']);
 
-Route::middleware(['auth:sanctum'])->group(function () {
+Route::middleware(['auth:sanctum', 'role:admin'])->group(function () {
     Route::prefix('tasks')->group(function () {
         Route::get('/', [AdminTaskController::class, 'index']);
         Route::get('/{task}', [AdminTaskController::class, 'show']);
         Route::post('/', [AdminTaskController::class, 'store']);
         Route::put('/{task}', [AdminTaskController::class, 'update']);
         Route::put('/{task}/status', [AdminTaskController::class, 'updateTaskStatus']);
-
         // Dependencies management
         Route::get('/{task}/dependencies', [AdminTaskController::class, 'getTaskDependencies']);
         Route::post('/{task}/dependencies', [AdminTaskController::class, 'addTaskDependencies']);
